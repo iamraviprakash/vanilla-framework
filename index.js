7beed00c62e72e2fda6a15f4674ccfcd063780d0
raviprakash.coder <raviprakash.coder@gmail.com>
2024-09-17 08:22:43 +0000

    chore: setup global store

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import store from "./services/store.js";
+
+window.app = {};
+app.store = store;
+
 window.addEventListener("DOMContentLoaded", () => {
     // Add code which will depend upon dom nodes here
 })
---
+++ services/store.js
@@ -0,0 +1,5 @@
+const store = {
+    states: [],
+}
+
+export default store;
